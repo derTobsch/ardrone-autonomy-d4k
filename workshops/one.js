d8028f851eb6eb14be0b7f2390f59a446334f678
Tobias Schneider <schneider@synyx.de>
2014-09-05 11:47:32 +0200

    change and adapt first workshop

--- workshops/one.js
+++ workshops/one.js
@@ -1,35 +1,54 @@
 var autonomy = require('../../ardrone-autonomy'),
     mission  = autonomy.createMission(),
-    arDrone = require('ar-drone'),
     arDroneConstants = require('ar-drone/lib/constants');
 
 function navdata_option_mask(c) {
-  return 1 << c;
+    return 1 << c;
 }
+
+// From the SDK.
+var navdata_options = (
+    navdata_option_mask(arDroneConstants.options.DEMO)
+    | navdata_option_mask(arDroneConstants.options.VISION_DETECT)
+    | navdata_option_mask(arDroneConstants.options.MAGNETO)
+    | navdata_option_mask(arDroneConstants.options.WIFI)
+    );
+
+mission.client().config('general:navdata_demo', true);
+mission.client().config('general:navdata_options', navdata_options);
 
 // Connect and configure the drone
 mission.client().config('control:outdoor', 0);
 mission.client().config('detect:detect_type', 12);
-mission.client().config('control:altitude_max', 1000);
-mission.client().config('control:altitude_min', 500);
-
-mission.log("mission-one.txt");
-
+mission.client().config('control:altitude_max', 3000);
+mission.client().config('control:altitude_min', 0);
 
 /* The kids can start here */
 
 mission.takeoff()
-       .forward(2.5)
-       .forward(0.5)
-       .land();
+    .zero()
+    .log()
+    .cw(30)
+    .log()
+    .forward(1.8)
+    .log()
+    .land();
 
 /* -- */
 
 mission.run(function (err, result) {
     if (err) {
-        console.trace("Oops, something bad happened: %s", err.message);
+
+        console.log("Oops, something bad happened: %s", err.message);
+        console.log('SecurityBox ',err.securityBox);
+        console.log('Goal ',err.goal);
+
         mission.client().stop();
         mission.client().land();
+
+        setTimeout(function () {
+            process.exit(0);
+        }, 1000);
     } else {
         console.log("We are done!");
         process.exit(0);
