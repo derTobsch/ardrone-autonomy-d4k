476cbd4ce031bf657115d96738c20b520a7c9717
Tobias Schneider <schneider@synyx.de>
2014-09-18 16:35:01 +0200

    add better battery outputs

--- src/batteryListener.js
+++ src/batteryListener.js
@@ -7,9 +7,14 @@ define(function () {
             client.on('navdata', function (navdata) {
                 if (navdata.demo && navdata.demo.batteryPercentage) {
                     var batteryPercentage = parseInt(navdata.demo.batteryPercentage);
-                    if (batteryPercentage != lastBatteryPercentage && batteryPercentage < 30 && (batteryPercentage % 5 === 0)) {
+
+                    if (batteryPercentage != lastBatteryPercentage && batteryPercentage <= 30 && (batteryPercentage % 3 === 0)) {
                         console.log('Die Batterie ist schwach: ', batteryPercentage + '%');
                         lastBatteryPercentage = batteryPercentage;
+                    }
+
+                    if (batteryPercentage <= 20) {
+                        console.log('Quadcopter startet nicht mehr');
                     }
                 }
             });
