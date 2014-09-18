01a6c1b2231ea4fdcb5432176ca3381ee2f53d5c
Tobias Schneider <schneider@synyx.de>
2014-09-18 16:35:27 +0200

    add try catch on workshoploader

--- starteMission.js
+++ starteMission.js
@@ -4,22 +4,28 @@ requirejs.config({
     nodeRequire: require
 });
 
-requirejs(['src/batteryListener', 'src/workshopLoader'],function (batteryListener, workshop) {
+requirejs(['src/batteryListener', 'src/workshopLoader'], function (batteryListener, workshop) {
 
         var autonomy = require('../ardrone-autonomy'),
             mission = autonomy.createMission(),
             arDroneConstants = require('ar-drone/lib/constants');
 
         batteryListener.listen(mission._client);
+
+        mission.client().config('general:navdata_demo', 'TRUE');
         
         // Connect and configure the drone
         mission.client().config('control:outdoor', 0);
         mission.client().config('control:altitude_max', 3000);
         mission.client().config('control:altitude_min', 0);
         mission.client().config('detect:detect_type', 12);
-        mission.client().config('general:navdata_demo', true);
-        
-        workshop.runWith(mission);
+
+        try {
+            workshop.runWith(mission);
+        }
+        catch (e) {
+            console.log(e.message);
+        }
 
         mission.run(function (err, result) {
             if (err) {
